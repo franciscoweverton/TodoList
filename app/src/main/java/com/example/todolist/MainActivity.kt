7c8f55d1b23d228a80096bb2e32b26a8ea22ca4d
Alunos <francisco.weverton.oliveira62@aluno.ifce.edu.br>
2023-03-29 18:32:01 -0300

    Novoprojeto

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,17 +2,28 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btnDeleteDoneTodos: Button
+    private lateinit var etTodoTitle: EditText
+    private lateinit var btnAddTodo: Button
+    private lateinit var rvTodoItems: RecyclerView
     private lateinit var todoAdapter: TodoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems = findViewById<RecyclerView>(R.id.rvTodoItems);
+        btnAddTodo = findViewById<Button>(R.id.btnAddTodo);
+        etTodoTitle = findViewById<EditText>(R.id.etTodoTitle);
+        btnDeleteDoneTodos = findViewById<Button>(R.id.btnDeleteDoneTodos);
 
         rvTodoItems.adapter = todoAdapter
         rvTodoItems.layoutManager = LinearLayoutManager(this)
--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -4,17 +4,23 @@ import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.item_todo.view.*
 
 class TodoAdapter(
     private val todos: MutableList<Todo>
 ) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
+    private lateinit var tvTodoTitle: TextView
+    private lateinit var cbDone: CheckBox
+
     class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+
         return TodoViewHolder(
             LayoutInflater.from(parent.context).inflate(
                 R.layout.item_todo,
@@ -46,6 +52,10 @@ class TodoAdapter(
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val curTodo = todos[position]
+
+        tvTodoTitle = holder.itemView.findViewById<TextView>(R.id.tvTodoTitle)
+        cbDone = holder.itemView.findViewById<CheckBox>(R.id.cbDone)
+
         holder.itemView.apply {
             tvTodoTitle.text = curTodo.title
             cbDone.isChecked = curTodo.isChecked
